439983f4ccfa2b3cbf3ed7b8b2a196b667323959
unknown <rboet001@gmail.com>
2018-10-15 18:08:16 -0400

    commit

---
+++ src/unit_tests/AccessControlServerTest.java
@@ -0,0 +1,64 @@
+package unit_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccessControlServerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAccessControlServer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartServer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReserveSpot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWrongUserDetected() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDuplicateIdFound() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsConnectionAvailable() {
+		fail("Not yet implemented");
+	}
+
+}
